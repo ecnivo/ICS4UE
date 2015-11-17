01933de726ef6f6ef2b2136b793498c15ad94e60
ecnivo <vinceou@gmail.com>
2015-11-17 14:03:39 -0500

    Sorting is a little less broken, I think.

--- src/unit4_exercise16_sorts_and_complexity/Sorts.java
+++ src/unit4_exercise16_sorts_and_complexity/Sorts.java
@@ -6,14 +6,14 @@ public class Sorts {
 
 	public static void main(String[] args) {
 		int[] numbers = generateRandomNumberArray(10);
-		long start = System.currentTimeMillis();
-		numbers = mergeSort(numbers);
-		long end = System.currentTimeMillis();
+		long start = System.nanoTime();
+		numbers = mergeSort(numbers, 0, numbers.length);
+		long end = System.nanoTime();
 
 		for (int i : numbers) {
-			System.out.println(i);
+			System.out.print(i + " ");
 		}
-		System.out.println("The operation took " + (end - start) + " millis");
+		System.out.println("The operation took " + (end - start) + " nanos");
 	}
 
 	public static int[] generateRandomNumberArray(int length) {
@@ -30,14 +30,60 @@ public class Sorts {
 		return returnArray;
 	}
 
-	public static int[] mergeSort(int[] array) {
-		if (array.length > 1) {
+	public static int[] mergeSort(int[] array, int start, int end) {
+		if (end - start <= 1)
+			return array;
 
+		// Finds middle and splits
+		int middle = (start + end) / 2;
+		mergeSort(array, start, middle);
+		mergeSort(array, middle, end);
+
+		// Prepares for merging
+		int[] returnArray = new int[array.length];
+		int frontCounter = 0;
+		int backCounter = middle;
+		int i = 0;
+		// Merges
+		while (i < returnArray.length && frontCounter < middle
+				&& backCounter < end) {
+			int nextVal = Integer.MAX_VALUE;
+			if (array[frontCounter] < array[backCounter]) {
+				nextVal = array[frontCounter];
+				frontCounter++;
+			} else {
+				nextVal = array[backCounter];
+				backCounter++;
+			}
+			returnArray[i] = nextVal;
+			i++;
+		}
+
+		// Merges the rest of the array if not covered
+		while (frontCounter < middle) {
+			returnArray[i] = array[frontCounter];
+			i++;
+			frontCounter++;
+		}
+		while (backCounter < end) {
+			returnArray[i] = array[backCounter];
+			i++;
+			backCounter++;
+		}
+
+		return returnArray;
+	}
+
+	public static int[] mergeSort(int[] array) {
+		// Splits array
+		if (array.length > 1) {
 			int middle = (int) (array.length / 2);
 			int[] front = mergeSort(Arrays.copyOfRange(array, 0, middle));
 			int[] back = mergeSort(Arrays.copyOfRange(array, middle,
 					array.length - 1));
 
+			// Finds the larger number in each of the arrays to put in the
+			// return
 			int[] returnArray = new int[array.length];
 			int frontCounter = 0;
 			int backCounter = 0;
@@ -56,19 +102,21 @@ public class Sorts {
 				i++;
 			}
 
-			while (backCounter < back.length - 1) {
-				returnArray[i] = back[backCounter];
-				i++;
-				backCounter++;
-			}
+			// Copies the remaining arrays over
 			while (frontCounter < front.length - 1) {
 				returnArray[i] = front[frontCounter];
 				i++;
 				frontCounter++;
 			}
+			while (backCounter < back.length - 1) {
+				returnArray[i] = back[backCounter];
+				i++;
+				backCounter++;
+			}
 
 			return returnArray;
 		}
+		// Returns the single number if not applicable
 		return array;
 
 	}
