198dd3a787ab38f27cc8e59c8466a97f912395f1
Vince Ou <vince.ou@gmail.com>
2015-11-18 21:21:58 -0500

    Everything is incredibly broken.

--- src/unit4_exercise16_sorts_and_complexity/Sorts.java
+++ src/unit4_exercise16_sorts_and_complexity/Sorts.java
@@ -8,7 +8,7 @@ public class Sorts {
 		// int[] numbers = generateRandomNumberArray(10);
 		int[] numbers = { 6, 2, 5, 0, 4, 3, 1 };
 		long start = System.nanoTime();
-		numbers = insertSort(numbers);
+		numbers = quickSort(numbers);
 		long end = System.nanoTime();
 
 		printArray(numbers);
@@ -83,7 +83,8 @@ public class Sorts {
 			int compare = curr;
 			while (array[compare] >= array[curr] && compare >= 1) {
 				compare--;
-				System.out.println("comparing " + array[curr] + " with " + array[compare]);
+				System.out.println("comparing " + array[curr] + " with "
+						+ array[compare]);
 			}
 			int moving = array[curr];
 
@@ -102,6 +103,64 @@ public class Sorts {
 			array[i] = array[i - 1];
 		}
 		array[index] = value;
+		return array;
+	}
+
+	public static int[] quickSort(int[] array) {
+		return quickSort(array, array.length);
+	}
+
+	public static int[] quickSort(int[] array, int usableLength) {
+		if (usableLength == 2) {
+			if (array[0] > array[1])
+				return new int[] { array[1], array[0] };
+			else
+				return array;
+		} else if (usableLength == 1) {
+			return array;
+		} else if (usableLength == 0) {
+			return new int[0];
+		}
+
+		int pivot = array[usableLength / 2];
+		int pivotOccur = 1;
+
+		int[] front = new int[array.length];
+		int[] back = new int[array.length];
+
+		int frontI = 0;
+		int backI = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < pivot) {
+				front[frontI] = array[i];
+				frontI++;
+			} else if (array[i] > pivot) {
+				back[backI] = array[i];
+				backI++;
+			} else
+				pivotOccur++;
+		}
+
+		front = quickSort(front, frontI);
+		back = quickSort(back, backI);
+
+		int i = 0;
+		while (i < array.length && i < front.length) {
+			array[i] = front[i];
+			i++;
+		}
+		int q = 0;
+		while (q < pivotOccur) {
+			array[i] = pivot;
+			i++;
+			q++;
+		}
+		int middle = i;
+		i = 0;
+		while (i < back.length) {
+			array[middle + i] = back[i];
+		}
+
 		return array;
 	}
 
