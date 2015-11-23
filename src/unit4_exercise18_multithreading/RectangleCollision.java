911a98bc04341a91647ef04619b1387d546bb2d0
Vince Ou <vince.ou@gmail.com>
2015-11-23 15:30:08 -0500

    more work on RectangleCollision

--- src/unit4_exercise18_multithreading/RectangleCollision.java
+++ src/unit4_exercise18_multithreading/RectangleCollision.java
@@ -6,15 +6,21 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import quicktime.app.event.MouseMoveListener;
+import quicktime.app.event.QTMouseEvent;
+
 public class RectangleCollision extends JFrame {
 
 	private static final int MOVE_AMOUNT = 3;
-	private static final int SQUARE_SIZE = 50;
+	private final static int SQUARE_SIZE = 50;
 
 	public static void main(String[] args) {
 		new RectangleCollision();
@@ -22,7 +28,7 @@ public class RectangleCollision extends JFrame {
 
 	private RectangleCollision() {
 		super("Rectangle Collision");
-		this.setPreferredSize(new Dimension(500, 500));
+		this.setPreferredSize(new Dimension(900, 500));
 		this.setLocation(100, 100);
 		this.add(new ContentPanel());
 		this.setVisible(true);
@@ -42,82 +48,81 @@ public class RectangleCollision extends JFrame {
 					(int) (Math.random() * RectangleCollision.this.getHeight()),
 					SQUARE_SIZE, SQUARE_SIZE);
 			boxArray.add(new BoxAI());
-			this.addKeyListener(new KeyListener() {
-
+			this.setBackground(Color.WHITE);
+			this.addMouseMotionListener(new MouseMotionListener() {
+				
 				@Override
-				public void keyTyped(KeyEvent e) {
-					// nothing
+				public void mouseMoved(MouseEvent e) {
+					double targetX = e.getX();
+					double targetY = e.getY();
+
+					// TODO make it follow the mouse!!!
 				}
-
+				
 				@Override
-				public void keyReleased(KeyEvent e) {
-					// nothing
-				}
-
-				@Override
-				public void keyPressed(KeyEvent e) {
-					if (e.getKeyCode() == KeyEvent.VK_DOWN)
-						userRect.y += MOVE_AMOUNT;
-					else if (e.getKeyCode() == KeyEvent.VK_UP)
-						userRect.y -= MOVE_AMOUNT;
-					else if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-						userRect.x += MOVE_AMOUNT;
-					else if (e.getKeyCode() == KeyEvent.VK_LEFT)
-						userRect.x -= MOVE_AMOUNT;
-					else if (e.getKeyCode() == KeyEvent.VK_O) {
-						boxArray.add(new BoxAI());
-					}
-					repaint();
+				public void mouseDragged(MouseEvent e) {
+// nothing					
 				}
 			});
 		}
 
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			g.setColor(Color.RED);
+			g.setColor(new Color(255,0,0,200));
 			g.fillRect(userRect.x, userRect.y, userRect.width, userRect.height);
 
 			g.setColor(Color.GRAY);
 			for (int i = 0; i < boxArray.size(); i++) {
-				g.fillRect(boxArray.get(i).x, boxArray.get(i).y,
-						boxArray.get(i).width, boxArray.get(i).height);
+				g.setColor(boxArray.get(i).colour);
+				g.fillRect((int) Math.round(boxArray.get(i).x),
+						(int) Math.round(boxArray.get(i).y), SQUARE_SIZE,
+						SQUARE_SIZE);
 			}
 		}
 
-		private class BoxAI extends Rectangle {
+		private class BoxAI {
 
-			private static final int MOVE_MULTIPLIER = 100;
-			private static final int SMOOTHNESS = 100;
-			private static final double SPEED = 1.0;
+			private static final double MOVE_MULTIPLIER = 100;
+			private static final double STEPS = 100;
+			private static final double SPEED = 5.0;
+
+			private double x;
+			private double y;
+
+			private Color colour;
 
 			private BoxAI() {
-				super(
-						(int) (Math.random() * RectangleCollision.this
-								.getWidth()),
-						(int) (Math.random() * RectangleCollision.this
-								.getHeight()), SQUARE_SIZE, SQUARE_SIZE);
+				x = (Math.random() * RectangleCollision.this.getWidth());
+				y = (Math.random() * RectangleCollision.this.getHeight());
+				colour = new Color((int) (Math.random() * 255),
+						(int) (Math.random() * 255),
+						(int) (Math.random() * 255), 200);
 				Thread t = new Thread(new Runnable() {
 
 					@Override
 					public void run() {
 						while (true) {
-
-							int deltaX = (int) (Math.random() * MOVE_MULTIPLIER - (MOVE_MULTIPLIER / 2));
-							int deltaY = (int) (Math.random() * MOVE_MULTIPLIER - (MOVE_MULTIPLIER / 2));
+							int deltaX = getDelta();
+							int deltaY = getDelta();
 
 							while (BoxAI.this.x + deltaX < 0
 									|| BoxAI.this.x + deltaX > RectangleCollision.this
 											.getWidth())
-								deltaX = (int) (Math.random() * MOVE_MULTIPLIER - (MOVE_MULTIPLIER / 2));
+								deltaX = getDelta();
 							while (BoxAI.this.y + deltaY < 0
 									|| BoxAI.this.y + deltaY > RectangleCollision.this
 											.getHeight())
-								deltaY = (int) (Math.random() * MOVE_MULTIPLIER - (MOVE_MULTIPLIER / 2));
+								deltaY = getDelta();
 
-							for (int step = 0; step < SMOOTHNESS; step++) {
+							double xStep = deltaX / STEPS;
+							double yStep = deltaY / STEPS;
+
+							for (int step = 0; step < STEPS; step++) {
+								BoxAI.this.x += xStep;
+								BoxAI.this.y += yStep;
 
 								try {
-									Thread.sleep(50);
+									Thread.sleep(Math.round(10 / SPEED));
 								} catch (InterruptedException e) {
 									e.printStackTrace();
 								}
@@ -128,6 +133,10 @@ public class RectangleCollision extends JFrame {
 					}
 				});
 				t.start();
+			}
+
+			private int getDelta() {
+				return (int) (Math.random() * MOVE_MULTIPLIER - (MOVE_MULTIPLIER / 2));
 			}
 		}
 	}
