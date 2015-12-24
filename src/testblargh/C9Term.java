280651394d2c6e8a2475a649259543004001bf27
Vince <vinceou@gmail.com>
2015-12-24 16:20:41 +0000

    Testing c9's console

---
+++ src/testblargh/C9Term.java
@@ -0,0 +1,8 @@
+package  testblargh;
+
+class C9Term {
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println(keyboard.getNextLine());
+    }
+}
