4839d948d80113fc1b1162f4737cd8547bac5e14
Vince <vinceou@gmail.com>
2015-12-24 16:21:50 +0000

    Something weird happened

--- src/testblargh/C9Term.java
+++ src/testblargh/C9Term.java
@@ -1,8 +1,10 @@
 package  testblargh;
 
+import java.util.Scanner;
+
 class C9Term {
     public static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
-        System.out.println(keyboard.getNextLine());
+        System.out.println(keyboard.nextLine());
     }
 }
